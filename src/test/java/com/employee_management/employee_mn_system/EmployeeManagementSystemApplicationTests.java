9ae95edbc28db55e7a3b42f295c840f5da48d941
tharusha <theenutheekshana@gmail.com>
2024-12-05 10:23:56 +0530

    Initial commit

---
+++ src/test/java/com/employee_management/employee_mn_system/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee_management.employee_mn_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
